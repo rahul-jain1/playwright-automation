469bad2eaea322fabcc7a8c32f7d866015670a7e
Rahul Jain <rahul_22452@yahoo.com>
2024-04-08 14:43:07 +1000

    Update commit

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    headless : false,
+    headless : true,
     screenshot : 'only-on-failure',
     viewport : {width:800, height:800},
     ignoreHTTPSErrors: false,
